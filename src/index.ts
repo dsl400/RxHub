46f3a45d6d182d577f4dba64a2649e374abeea5e
Valentin Sasek <dsl400@gmail.com>
2023-11-20 08:45:27 +0200

    add ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { RxHub } from './RxHub'
 import { RxHubDriver } from './RxHub.Driver'
 import { RxHubAuth } from './RxHub.Auth'
 
+
 export * from './types'
 export default {
     RxHub, RxHubDriver, RxHubAuth
